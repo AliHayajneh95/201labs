e35ee46f8b40b3d2e0e4d9bf9afd2060c0075eb7
Ali <Hayajnehali95@gmail.com>
2020-05-13 00:51:03 +0300

    lab 02 done

---
+++ JS/script.js
@@ -0,0 +1,50 @@
+var i = true;
+while (i)
+{
+    var str = prompt('his hair color is black or white');
+    if(str == 'black')
+    {
+        alert('right!')
+        i = false;
+    } 
+    else
+        alert('wronge!')
+}
+i = true;
+while (i)
+{
+    var str = prompt('he has a bach in cs or engineering');
+    if(str == 'cs')
+    {
+        alert('right!')
+        i = false;
+    }
+    else
+        alert('wronge!')
+}
+i = true;
+while (i)
+{
+    var str = prompt('he is 25 or 30 years old?');
+    if(str == '25')
+    {
+        alert('right!')
+        i = false;
+    }
+    else
+        alert('wronge!')
+}
+i = true;
+while (i)
+{
+    var str = prompt('he eats so much food, yes/no?');
+    if(str == 'no')
+    {
+        alert('right!')
+        i = false;
+    }
+    else
+        alert('wronge!')
+}
+
+document.getElementsByClassName("hide").style.display = "Block";
