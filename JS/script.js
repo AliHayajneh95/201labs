a4259317eb565db373df635bc4015a9f40a04fab
Ali <Hayajnehali95@gmail.com>
2020-05-13 23:12:28 +0300

    Lab02 done

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,14 @@
+'use strict';
+            
 var i = true;
 while (i)
 {
     var str = prompt('his hair color is black or white');
-    if(str == 'black')
+    if(str.toLowerCase == 'black')
     {
         alert('right!')
         i = false;
+        console.log(str);
     } 
     else
         alert('wronge!')
@@ -14,10 +17,11 @@ i = true;
 while (i)
 {
     var str = prompt('he has a bach in cs or engineering');
-    if(str == 'cs')
+    if(str.toLowerCase == 'cs')
     {
         alert('right!')
         i = false;
+        console.log(str);
     }
     else
         alert('wronge!')
@@ -26,10 +30,11 @@ i = true;
 while (i)
 {
     var str = prompt('he is 25 or 30 years old?');
-    if(str == '25')
+    if(str.toLowerCase == '25')
     {
         alert('right!')
         i = false;
+        console.log(str);
     }
     else
         alert('wronge!')
@@ -38,13 +43,22 @@ i = true;
 while (i)
 {
     var str = prompt('he eats so much food, yes/no?');
-    if(str == 'no')
+    if(str.toLowerCase == 'no')
     {
         alert('right!')
         i = false;
+        console.log(str);
     }
     else
         alert('wronge!')
 }
 
-document.getElementsByClassName("hide").style.display = "Block";
+var elements = document.getElementsByClassName("hide");
+var x = elements.length
+
+while(x != 0)
+{
+    elements.item(x).style.display = "block";
+    
+    x--;
+}
