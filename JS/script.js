c7e88e8ceebba4c440675229bbb884eab3fabf7d
Ali <Hayajnehali95@gmail.com>
2020-05-13 23:27:33 +0300

    Lab02 is finally done

--- JS/script.js
+++ JS/script.js
@@ -4,7 +4,7 @@ var i = true;
 while (i)
 {
     var str = prompt('his hair color is black or white');
-    if(str.toLowerCase == 'black')
+    if(str.toLowerCase() == 'black')
     {
         alert('right!')
         i = false;
@@ -17,7 +17,7 @@ i = true;
 while (i)
 {
     var str = prompt('he has a bach in cs or engineering');
-    if(str.toLowerCase == 'cs')
+    if(str.toLowerCase() == 'cs')
     {
         alert('right!')
         i = false;
@@ -30,7 +30,7 @@ i = true;
 while (i)
 {
     var str = prompt('he is 25 or 30 years old?');
-    if(str.toLowerCase == '25')
+    if(str.toLowerCase() == '25')
     {
         alert('right!')
         i = false;
@@ -43,7 +43,7 @@ i = true;
 while (i)
 {
     var str = prompt('he eats so much food, yes/no?');
-    if(str.toLowerCase == 'no')
+    if(str.toLowerCase() == 'no')
     {
         alert('right!')
         i = false;
@@ -59,6 +59,6 @@ var x = elements.length
 while(x != 0)
 {
     elements.item(x).style.display = "block";
-    
+
     x--;
 }
